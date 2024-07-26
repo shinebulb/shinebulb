c0f8d3afe3a9042c20c175b568d3f5d2ffc417b6
Nurgali <75561812+nurgalinchik@users.noreply.github.com>
2024-07-26 15:02:38 +1000

    Update ThemeConstructor.jsx

--- src/ThemeConstructor.jsx
+++ src/ThemeConstructor.jsx
@@ -1,26 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import text from './assets/json/text.json';
 import themes from './assets/themes';
 
-function ThemeConstructor({reference, themeState}) {
-
+function ThemeConstructor({ reference, themeState }) {
     const lang = parseInt(localStorage.getItem("langMode")) || 0;
-    const theme = parseInt(localStorage.getItem("theme")) || 0;
 
     const [localBg, setLocalBg] = useState(localStorage.getItem("bg") || "#2e5a97");
     const [localFont, setLocalFont] = useState(localStorage.getItem("font") || "#f1f1f1");
-    
+
     function customTheme() {
         document.body.classList.add('theme-transition');
-        setTimeout(() => {
-            document.body.classList.remove('theme-transition');
-        }, 100);
+            setTimeout(() => {
+                document.body.classList.remove('theme-transition');
+            }, 100);
         themeState(3);
         reference.current.close();
         localStorage.setItem("theme", 3);
         localStorage.setItem("bg", localBg);
         localStorage.setItem("font", localFont);
-        themes[theme]();
+        themes[3]();
     }
 
     return (
@@ -28,31 +26,31 @@ function ThemeConstructor({reference, themeState}) {
             <div className="themeHeader">
                 <div>
                     <label>
-                        {text[lang].customTheme[0]}<br/>
+                        {text[lang].customTheme[0]}<br />
                         <span>{`(${text[lang].current}: ${localBg})`}</span>
                     </label>
-                    <input type="color" value={localBg} onChange={event => setLocalBg(event.target.value)}/>
+                    <input type="color" value={localBg} onChange={event => setLocalBg(event.target.value)} />
                 </div>
                 <div>
                     <label>
-                        {text[lang].customTheme[1]}<br/>
+                        {text[lang].customTheme[1]}<br />
                         <span>{`(${text[lang].current}: ${localFont})`}</span>
                     </label>
-                    <input type="color" value={localFont} onChange={event => setLocalFont(event.target.value)}/>
+                    <input type="color" value={localFont} onChange={event => setLocalFont(event.target.value)} />
                 </div>
             </div>
             <hr />
-            <div className="sample" style={{backgroundColor: localBg, color: localFont}}>
+            <div className="sample" style={{ backgroundColor: localBg, color: localFont }}>
                 <p>{text[lang].sample}</p>
-                <button onClick={customTheme} style={{color: localFont, border: `${localFont} 3px solid`}}>
+                <button onClick={customTheme} style={{ color: localFont, border: `${localFont} 3px solid` }}>
                     {text[lang].themeControls[0]}
                 </button>
-                <button onClick={() => reference.current.close()} style={{color: localFont, border: `${localFont} 3px solid`}}>
+                <button onClick={() => reference.current.close()} style={{ color: localFont, border: `${localFont} 3px solid` }}>
                     {text[lang].themeControls[1]}
                 </button>
             </div>
         </dialog>
-    )
+    );
 }
 
-export default ThemeConstructor
+export default ThemeConstructor;
