c912842f1b35292f0bd106e07ba8f7c707aeb4f6
Andrew <aldortheold@gmail.com>
2024-07-31 22:31:54 +1000

    modified theme suggestions

--- src/ThemeSuggestion.jsx
+++ src/ThemeSuggestion.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import themes from './assets/themes'
 import text from './assets/json/text.json';
 import paths from './assets/json/svg-paths.json';
@@ -8,7 +8,9 @@ function ThemeSuggestion({themeIndex}) {
     
     const lang = parseInt(localStorage.getItem("langMode")) || 0;
 
-    const [hide, setHide] = useState("block");
+    const [hide, setHide] = useState(localStorage.getItem(`display #${themeIndex + 1}`) || "block");
+
+    const hideRef = useRef(null);
 
     function paintTheme() {
         localStorage.setItem("theme", 3);
@@ -21,19 +23,27 @@ function ThemeSuggestion({themeIndex}) {
         themes[3]();
     }
 
+    function hideTheme() {
+        setHide("none");
+        localStorage.setItem(`display #${themeIndex + 1}`, "none");
+    }
+
     return (
         <div className="theme-card" style={{display: hide, backgroundColor: colors[themeIndex][0]}}>
             <p style={{color: colors[themeIndex][1]}}>{text[lang].names[themeIndex]}</p>
             <div className="saved-controls">
-                <button style={{backgroundColor: colors[themeIndex][0], border: `${colors[themeIndex][1]} 3px solid`}}>
-                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d={paths.save} stroke={colors[themeIndex][1]} strokeWidth="2" strokeLinejoin="round"/></svg>
-                </button>
                 <button onClick={paintTheme} style={{backgroundColor: colors[themeIndex][0], border: `${colors[themeIndex][1]} 3px solid`}}>
                     <svg fill={colors[themeIndex][1]} version="1.1" id="Uploaded to svgrepo.com" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xmlSpace="preserve"><path className="bentblocks_een" d={paths.paint}/></svg>
                 </button>
-                <button onClick={() => setHide("none")} style={{backgroundColor: colors[themeIndex][0], border: `${colors[themeIndex][1]} 3px solid`}}>
+                <button onClick={() => hideRef.current.showModal()} style={{backgroundColor: colors[themeIndex][0], border: `${colors[themeIndex][1]} 3px solid`}}>
                     <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path d={paths.hide[0]} fill={colors[themeIndex][1]}/><path d={paths.hide[1]} fill={colors[themeIndex][1]}/></svg>
                 </button>
+                
+                <dialog ref={hideRef} className="confirm">
+                    <p>{text[lang].hideTheme}</p>
+                    <button onClick={hideTheme}>{text[lang].confirm[1]}</button>
+                    <button onClick={() => hideRef.current.close()}>{text[lang].confirm[2]}</button>
+                </dialog>
             </div>
         </div>
     )
