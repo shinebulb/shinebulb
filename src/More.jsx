5085ff8ee103c2be30ae467944e2bc9794d429f5
Andrew <aldortheold@gmail.com>
2024-08-01 15:14:32 +1000

    modified /saved & /explore links

--- src/More.jsx
+++ src/More.jsx
@@ -1,4 +1,5 @@
 import text from './assets/json/text.json';
+import paths from './assets/json/svg-paths.json';
 
 function More({ options }) {
 
@@ -8,12 +9,18 @@ function More({ options }) {
         <dialog className="more" ref={options}>
             <div className="options">
                 <a href="/saved">
-                    {text[lang].saved}
+                    <p>
+                        <svg style={{height: "2rem"}} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d={paths.save} strokeWidth="2" strokeLinejoin="round"/></svg>
+                        {text[lang].saved}
+                    </p>
                     <span>{text[lang].optionDescriptions[0]}</span>
                 </a>
                 <hr />
                 <a href="/explore">
-                    {text[lang].themeSuggestions}
+                    <p>
+                        <svg style={{height: "2.5rem"}} viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path d={paths.explore} strokeWidth="1.2"/></svg>
+                        {text[lang].themeSuggestions}
+                    </p>
                     <span>{text[lang].optionDescriptions[1]}</span>
                 </a>
                 <hr />
