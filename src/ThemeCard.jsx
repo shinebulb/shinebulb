27f4108973abdd0f3932e93d1e9b8e46a26026cc
Andrew <aldortheold@gmail.com>
2024-08-09 00:29:17 +1000

    Revert "added custom button styling options"
    This reverts commit be500dcef106a4d2a9550eed90617903aa591fdf.

--- src/ThemeCard.jsx
+++ src/ThemeCard.jsx
@@ -1,5 +1,4 @@
-import React, {useState, useRef} from 'react';
-import hsl from 'hex-to-hsl';
+import React, {useRef} from 'react';
 import themes from './assets/themes';
 import paths from './assets/json/svg-paths.json';
 import text from './assets/json/text.json';
@@ -7,8 +6,6 @@ import text from './assets/json/text.json';
 function ThemeCard({savedState, nameStates, savedUpdate, nameUpdates, themeIndex}) {
 
     const lang = parseInt(localStorage.getItem("langMode")) || 0;
-    
-    const [hover, setHover] = useState([false, false, false]);
 
     const renameRef = useRef(null);
     const inputRef = useRef(null);
@@ -46,44 +43,32 @@ function ThemeCard({savedState, nameStates, savedUpdate, nameUpdates, themeIndex
                 <button
                     title={text[lang].themeCard[1]}
                     style={{
-                        backgroundColor: hover[0] ? `hsl(${hsl(savedState[themeIndex][0])[0]}, ${hsl(savedState[themeIndex][0])[1]}%, ${savedState[themeIndex][4]}%)` : savedState[themeIndex][2],
-                        border: `${savedState[themeIndex][3]} 3px solid`
+                        backgroundColor: savedState[themeIndex][0],
+                        border: `${savedState[themeIndex][1]} 3px solid`
                     }}
                     onClick={() => renameRef.current.showModal()}
-                    onMouseEnter={() => {
-                        if (savedState[themeIndex][4] != hsl(savedState[themeIndex][0])[2]) setHover([true, false, false]);
-                    }}
-                    onMouseLeave={() => setHover([false, false, false])}
                 >
-                    <svg fill={savedState[themeIndex][3]} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d={paths.rename[0]}/><path fillRule="evenodd" clipRule="evenodd" d={paths.rename[1]}/></svg>
+                    <svg fill={savedState[themeIndex][1]} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d={paths.rename[0]}/><path fillRule="evenodd" clipRule="evenodd" d={paths.rename[1]}/></svg>
                 </button>
                 <button
                     title={text[lang].themeCard[2]}
                     style={{
-                        backgroundColor: hover[1] ? `hsl(${hsl(savedState[themeIndex][0])[0]}, ${hsl(savedState[themeIndex][0])[1]}%, ${savedState[themeIndex][4]}%)` : savedState[themeIndex][2],
-                        border: `${savedState[themeIndex][3]} 3px solid`
+                        backgroundColor: savedState[themeIndex][0],
+                        border: `${savedState[themeIndex][1]} 3px solid`
                     }}
                     onClick={paintTheme}
-                    onMouseEnter={() => {
-                        if (savedState[themeIndex][4] != hsl(savedState[themeIndex][0])[2]) setHover([false, true, false]);
-                    }}
-                    onMouseLeave={() => setHover([false, false, false])}
                 >
-                    <svg fill={savedState[themeIndex][3]} version="1.1" id="Uploaded to svgrepo.com" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xmlSpace="preserve"><path className="bentblocks_een" d={paths.paint}/></svg>
+                    <svg fill={savedState[themeIndex][1]} version="1.1" id="Uploaded to svgrepo.com" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xmlSpace="preserve"><path className="bentblocks_een" d={paths.paint}/></svg>
                 </button>
                 <button
                     title={text[lang].themeCard[3]}
                     style={{
-                        backgroundColor: hover[2] ? `hsl(${hsl(savedState[themeIndex][0])[0]}, ${hsl(savedState[themeIndex][0])[1]}%, ${savedState[themeIndex][4]}%)` : savedState[themeIndex][2],
-                        border: `${savedState[themeIndex][3]} 3px solid`
+                        backgroundColor: savedState[themeIndex][0],
+                        border: `${savedState[themeIndex][1]} 3px solid`
                     }}
                     onClick={() => deleteRef.current.showModal()}
-                    onMouseEnter={() => {
-                        if (savedState[themeIndex][4] != hsl(savedState[themeIndex][0])[2]) setHover([false, false, true]);
-                    }}
-                    onMouseLeave={() => setHover([false, false, false])}
                 >
-                    <svg fill={savedState[themeIndex][3]} version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 482.428 482.429" xmlSpace="preserve"><g><g><path d={paths.delete[0]}/><path d={paths.delete[1]}/><path d={paths.delete[2]}/><path d={paths.delete[3]}/></g></g></svg>
+                    <svg fill={savedState[themeIndex][1]} version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 482.428 482.429" xmlSpace="preserve"><g><g><path d={paths.delete[0]}/><path d={paths.delete[1]}/><path d={paths.delete[2]}/><path d={paths.delete[3]}/></g></g></svg>
                 </button>
                 
                 <dialog ref={renameRef} className="confirm">
